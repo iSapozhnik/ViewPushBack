58b91b9b644a45b1df92ef4ca448e12a76b9b505
Sapozhnik Ivan <sapozhnik.ivan@gmail.com>
2013-01-13 17:51:41 +0200

    snall fix

--- Trapezoid/ViewController.m
+++ Trapezoid/ViewController.m
@@ -11,7 +11,9 @@
 
 static const float animationSpeed = 0.2;
 
-@interface ViewController ()
+@interface ViewController () {
+    BOOL isOnBack;
+}
 
 typedef void(^Completion)(BOOL finished);
 @property (nonatomic, strong) UIView *flipView;
@@ -30,11 +32,13 @@ typedef void(^Completion)(BOOL finished);
     [self.view sendSubviewToBack:self.flipView];
     
     self.flipView.layer.shadowOpacity = 0.01;
+    
+    isOnBack = NO;
 }
 
 - (IBAction)playAnimation:(id)sender
 {
-    [self goAction:self.flipView completion:^(BOOL finished) {
+    [self goBack:self.flipView completion:^(BOOL finished) {
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Info" message:@"View moved to back" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
         [alert show];
     }];
@@ -42,42 +46,52 @@ typedef void(^Completion)(BOOL finished);
 
 - (IBAction)backAnimation:(id)sender
 {
-    [self undoAction:self.flipView completion:^(BOOL finished) {
+    [self goFront:self.flipView completion:^(BOOL finished) {
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Info" message:@"View moved to front" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
         [alert show];
     }];
 }
 
-- (void)goAction:(UIView *)view completion:(Completion)handler
+- (void)goBack:(UIView *)view completion:(Completion)handler
 {
-    [UIView animateWithDuration:animationSpeed delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-        CALayer *layer = view.layer;
-        CATransform3D transform = CATransform3DIdentity;
-        transform.m34 = 1.0 / -300;
-        layer.transform = CATransform3DRotate(transform, 10.0f * M_PI / 180.0f, 1.0f, 0.0f, 0.0f);
-    } completion:^(BOOL finished) {
-        [UIView animateWithDuration:animationSpeed animations:^{
-            view.transform = CGAffineTransformMakeScale(0.85, 0.85);
+    if (isOnBack == NO) {
+        [UIView animateWithDuration:animationSpeed delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            CALayer *layer = view.layer;
+            CATransform3D transform = CATransform3DIdentity;
+            transform.m34 = 1.0 / -300;
+            layer.transform = CATransform3DRotate(transform, 10.0f * M_PI / 180.0f, 1.0f, 0.0f, 0.0f);
         } completion:^(BOOL finished) {
-            handler(YES);
+            [UIView animateWithDuration:animationSpeed animations:^{
+                view.transform = CGAffineTransformMakeScale(0.85, 0.85);
+            } completion:^(BOOL finished) {
+                isOnBack = YES;
+                handler(YES);
+            }];
         }];
-    }];
+    } else {
+        return;
+    }
 }
 
-- (void)undoAction:(UIView *)view completion:(Completion)handler
+- (void)goFront:(UIView *)view completion:(Completion)handler
 {
-    [UIView animateWithDuration:animationSpeed delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-        CALayer *layer = view.layer;
-        CATransform3D transform = CATransform3DIdentity;
-        transform.m34 = 1.0 / 300;
-        layer.transform = CATransform3DRotate(transform, -10.0f * M_PI / 180.0f, 1.0f, 0.0f, 0.0f);
-    } completion:^(BOOL finished) {
-        [UIView animateWithDuration:animationSpeed animations:^{
-            view.transform = CGAffineTransformMakeScale(1.0, 1.0);
+    if (isOnBack == YES) {
+        [UIView animateWithDuration:animationSpeed delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            CALayer *layer = view.layer;
+            CATransform3D transform = CATransform3DIdentity;
+            transform.m34 = 1.0 / 300;
+            layer.transform = CATransform3DRotate(transform, -10.0f * M_PI / 180.0f, 1.0f, 0.0f, 0.0f);
         } completion:^(BOOL finished) {
-            handler(YES);
+            [UIView animateWithDuration:animationSpeed animations:^{
+                view.transform = CGAffineTransformMakeScale(1.0, 1.0);
+            } completion:^(BOOL finished) {
+                isOnBack = NO;
+                handler(YES);
+            }];
         }];
-    }];
+    } else {
+        return;
+    }
 }
 
 @end
